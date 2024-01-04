69c3e597cf96ad5b76288ba967225076dcfcdfa1
shazaaly <shaza.aly@gmail.com>
2024-01-05 01:00:14 +0200

    File: 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """Pascal Triangle building function"""
+    if n <= 0:
+        return []
+
+    def helper(row, col):
+        """recurs"""
+        if col == 0 or col == row:
+            return 1
+        return helper(row - 1, col - 1) + helper(row - 1, col)
+
+    triangle = []
+
+    for i in range(n):
+        curr_row = []
+        for j in range(i + 1):
+            curr_row.append(helper(i, j))
+        triangle.append(curr_row)
+
+    return triangle
+
+
