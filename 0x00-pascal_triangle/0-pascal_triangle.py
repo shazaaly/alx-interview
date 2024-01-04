f5922e9838fd41ec1519a535759d6d7b8964e684
shazaaly <shaza.aly@gmail.com>
2024-01-05 01:08:14 +0200

    1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,8 +17,9 @@ def pascal_triangle(n):
     for i in range(n):
         curr_row = []
         for j in range(i + 1):
-            if i > 0 and j > 0:
-                curr_row.append(helper(i, j))
+            if i == 0 and j == 0:
+                curr_row.append(1)
+            curr_row.append(helper(i, j))
         triangle.append(curr_row)
 
     return triangle
