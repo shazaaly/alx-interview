8feac084a3773a5b77ed608fc3fe4e7589967651
shazaaly <shaza.aly@gmail.com>
2024-01-10 00:56:12 +0200

    while

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,34 +2,14 @@
 
 def canUnlockAll(boxes):
     """Function to return true if all boxes unlocked """
-    unlocked_boxes = list(boxes[0])  # [1]
-
-    for i in range(len(boxes)):
-        for key in boxes[i]:
-            if i in unlocked_boxes and i > 0 and key > 0:
-                unlocked_boxes.append(key)
-
-    unlocked_boxes = list(set(unlocked_boxes))
-
-    if len(unlocked_boxes) == len(boxes):
-        return True
-    return False
+    unlocked_boxes = [0]
+    keys = list(boxes[0])
 
 
+    while keys:
+        key = keys.pop()
+        if key < len(boxes) and key not in unlocked_boxes:
+            unlocked_boxes.append(key)
+            keys.extend(boxes[key])
 
-    #return unlocked_boxes
-
-
-
-
-
-
-
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+        return len(unlocked_boxes) == len(boxes)
