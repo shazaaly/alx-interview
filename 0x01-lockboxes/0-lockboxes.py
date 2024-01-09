897c7fa7d40478ff89789d57406d811d12a6fc11
shazaaly <shaza.aly@gmail.com>
2024-01-10 00:57:40 +0200

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-    """Function to return true if all boxes unlocked """
-    unlocked_boxes = [0]
-    keys = list(boxes[0])
-
+    unlocked_boxes = [0]  # Box 0 is already unlocked
+    keys = list(boxes[0])  # Start with keys from Box 0
 
     while keys:
         key = keys.pop()
@@ -12,4 +10,14 @@ def canUnlockAll(boxes):
             unlocked_boxes.append(key)
             keys.extend(boxes[key])
 
-        return len(unlocked_boxes) == len(boxes)
+    return len(unlocked_boxes) == len(boxes)
+
+# Test cases
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))  # Expected output: False
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))  # Expected output: True
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))  # Expected output: False
