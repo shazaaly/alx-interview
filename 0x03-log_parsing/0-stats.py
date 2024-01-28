94f954870c1192a7b0a5476a352f6666b882e93b
shazaaly <shaza.aly@gmail.com>
2024-01-28 19:48:27 +0200

    except

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,66 +1,39 @@
 #!/usr/bin/python3
-"""a script that reads stdin line by line and computes metrics
-"""
-
-import sys, re
-
-lines = []
+import sys
+import re
 
 def use_regex(input_text):
     pattern = re.compile(r'^(\d{1,3}(\.\d{1,3}){3}) - (.*?) "GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)$')
     return pattern.match(input_text)
 
-
-try:
-    while True:
-        line = input()
-        lines.append(line)
-except EOFError:
-    pass
-except KeyboardInterrupt:
-    pass
-
 possible_status_codes = {
-    "200" : 0,
-    "301" : 0,
-    "400" : 0,
-    "401" : 0,
-    "403" : 0,
-    "404" : 0,
-    "405" : 0,
-    "500" : 0,
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
 }
 
 total_file_size = 0
-try:
 
-    for line in lines:
+try:
+    for line in sys.stdin:
         match = use_regex(line)
         if match:
-            ip_address = match.group(1)
-            date = match.group(3)
             status_code = match.group(4)
-            if str(status_code) in possible_status_codes:
+            if status_code in possible_status_codes:
                 possible_status_codes[status_code] += 1
 
-
-            file_size = match.group(5)
-            total_file_size += int(file_size)
-            #print(f"File size: {total_file_size}")
-except EOFError:
-    pass
+            file_size = int(match.group(5))
+            total_file_size += file_size
 except KeyboardInterrupt:
-    pass
-finally:
-    print(f"File size: {total_file_size}\t")
+    print(f"File size: {total_file_size}")
     for status_code, count in possible_status_codes.items():
         print(f"{status_code}: {count}")
-
-
-
-
-
-
-
-
-
+finally:
+    print(f"File size: {total_file_size}")
+    for status_code, count in possible_status_codes.items():
+        print(f"{status_code}: {count}")
