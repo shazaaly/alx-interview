ef3eafa98b869792a34344f8a50786045ddcd019
shazaaly <shaza.aly@gmail.com>
2024-02-02 02:10:45 +0200

    Update `validUTF8` function to handle multi-byte UTF-8 characters.

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,25 +4,25 @@
     set represents a valid UTF-8 encoding.
 """
 
-
 def validUTF8(data):
-    """ٌ Return  True if data is a valid UTF-8 encoding, else return False
+    """Return True if data is a valid UTF-8 encoding, else return False.
 
     Args:
-        data (_type_):  list of integers
+        data (List[int]): list of integers representing bytes in the data set.
     """
     count = 0
     for byte in data:
         if count == 0:
-            mask = 0x80
-            while (byte & mask) != 0:
-                count += 1
-                mask >>= 1
-            if count == 0:
-                continue
-            if count > 4 or count == 1:
+            if (byte >> 5) == 0b110:
+                count = 1
+            elif (byte >> 4) == 0b1110:
+                count = 2
+            elif (byte >> 3) == 0b11110:
+                count = 3
+            elif (byte >> 7):
                 return False
         else:
-            if (byte & 0xC0) != 0x80:
+            if (byte >> 6) != 0b10:
                 return False
+            count -= 1
     return count == 0
