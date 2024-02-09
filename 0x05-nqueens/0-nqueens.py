4bf115974b86f211c992c9d410ae86e8c2a0d1aa
shazaaly <shaza.aly@gmail.com>
2024-02-09 17:49:16 +0200

    Refactor variable names in nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -11,10 +11,10 @@ def backtrack(r, n, cols, pos, neg, board):
     """
     if r == n:
         res = []
-        for l in range(len(board)):
-            for k in range(len(board[l])):
-                if board[l][k] == 1:
-                    res.append([l, k])
+        for i in range(len(board)):
+            for k in range(len(board[i])):
+                if board[i][k] == 1:
+                    res.append([i, k])
         print(res)
         return
 
