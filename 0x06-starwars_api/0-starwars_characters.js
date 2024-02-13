d4b7038845b1f8d7563d07f0b14a7263a224799c
shazaaly <shaza.aly@gmail.com>
2024-02-13 19:23:44 +0200

    Remove unused variable and add console.log statement

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
 
 const findMovie = (id) => {
   const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
@@ -25,3 +24,4 @@ const findMovie = (id) => {
     }
   });
 };
+console.log(findMovie);
